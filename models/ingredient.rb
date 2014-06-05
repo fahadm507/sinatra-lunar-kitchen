8e7439ff22a9ddf1d43ef4d879257c0f965546b0
Fahad Mukunzi <fahadm507@gmail.com>
2014-06-05 17:31:45 -0400

    initial commit

--- models/ingredient.rb
+++ models/ingredient.rb
@@ -1,2 +1,22 @@
+
 class Ingredient
+  attr_reader :name, :recipe_id, :id
+  def initialize(ingredient)
+    @name = ingredient['name']
+    @recipe_id= ingredient['recipe_id']
+    @id = ingredient['id']
+  end
+
+  def self.find_ingredients(recipe_id)
+    connection = PG.connect(dbname: 'recipes')
+    query = connection.exec_params('SELECT * FROM ingredients WHERE recipe_id = $1', [recipe_id])
+    connection.close
+
+    ingredients = []
+
+    query.each do |ingredient|
+      ingredients << Ingredient.new(ingredient)
+    end
+    ingredients
+  end
 end
--- models/recipe.rb
+++ models/recipe.rb
@@ -1,2 +1,40 @@
 class Recipe
+  attr_reader :name, :id, :description, :instructions, :ingredients
+  def initialize(recipe)
+    @name = recipe['name']
+    @id = recipe['id']
+    @description = recipe['description']
+    @instructions = recipe['instructions']
+    @ingredients = recipe['ingredients']
+  end
+
+  def self.all
+
+    connection = PG.connect(dbname: 'recipes')
+    results= connection.exec('SELECT * FROM recipes ORDER BY name')
+    connection.close
+
+    @recipes = []
+
+    results.each do |recipe|
+      @recipes << Recipe.new(recipe)
+    end
+    @recipes
+  end
+
+  def self.find(params)
+    a_recipe = nil
+    @recipes.each do |recipe|
+      if recipe.id == params
+        a_recipe = recipe
+      end
+    end
+    a_recipe
+  end
+
+  def ingredients
+
+     @ingredients = Ingredient.find_ingredients(id)
+  end
+
 end
